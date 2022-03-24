f1510003ee4bbaee0ad1b053c465534bff6579db
ahmedmosh <ahmedmoshood100@gmail.com>
2022-03-24 15:58:31 +0100

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,6 @@
-main.h
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _main_H_
+#define _main_H_
 
-int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -13,6 +11,7 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+int _putchar(char);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
